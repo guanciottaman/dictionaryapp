2453c1b1fd9252c2fa38d5a7fd5934189a23a47a
Guanciottaman <italia.matteo00@gmail.com>
2023-05-19 12:19:06 +0200

    Added comments

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class App(CTk):
     def search_word(self):
         """Search for a word in FreeDictionaryAPI"""
         try:
+            # URL for API request
             url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{self.entry.get()}'
             response = requests.get(url, timeout=10)
             json = response.json()
@@ -45,6 +46,7 @@ class App(CTk):
                         synonyms.append('None')
                     else:
                         synonyms.append(syn)
+                # if lenght is greater or equal than 5 make synonyms go in newlines
                 if len(synonyms) >= 5:
                     synonyms_text = '\n'.join(synon for synon in synonyms)
                 else:
@@ -61,7 +63,7 @@ class App(CTk):
                 definitions.insert('end', f'As {defin["partOfSpeech"]}:\n')
                 for definit in defin['definitions']:
                     definitions.insert('end', f'{definit["definition"]}')
-                definitions.insert('end', '\n -------------- \n')
+                definitions.insert('end', '\n -------------- \n') # spacing
             definitions.grid(row=1, column=1, padx=20, pady=20)
             definitions.configure(state='disabled')
 
@@ -87,13 +89,13 @@ class App(CTk):
                 break
             audio_response = requests.get(audio_link, timeout=10)
             with open(f'{json[0]["word"]}.mp3', 'wb') as file_:
-                file_.write(audio_response.content)
+                file_.write(audio_response.content) # write the mp3 to disk in binary format
             playsound.playsound(f'{Path().cwd() / f"""{json[0]["word"]}.mp3"""}',
                                 block=False)
             os.remove(f'{json[0]["word"]}.mp3')
-        except requests.exceptions.MissingSchema:
+        except requests.exceptions.MissingSchema: # No audio link in the phonetics
             showerror('Word has no audio', 'The API did not provide audio for this word.')
-        except playsound.PlaysoundException:
+        except playsound.PlaysoundException: # Fixing error when playing multiple tracks
             os.system(f'{"main.exe" if "main.exe" in os.listdir() else "python main.py"}')
             sys.exit(0)
 
